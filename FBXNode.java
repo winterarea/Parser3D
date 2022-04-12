fc58f1a82170018a91bf0c16b2f70023f1da22d1
winterarea <87577641+winterarea@users.noreply.github.com>
2022-04-12 10:49:45 +0800

    Create FBXNode.java

---
+++ FBXNode.java
@@ -0,0 +1,35 @@
+package com.nmaid.asset.fbx;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FBXNode {
+	public boolean singleProperty;
+	public long id;
+	public String attrName;
+	public String attrType;
+	public String name;
+	public Object node;
+	public List<Object> propertyList;
+	public Map<String,Object> SubNodeMap;
+	public Object a;
+	public List<Object> connections;
+	
+	public FBXNode parent;
+	public List<FBXNode> children;
+	public FBXNode(){
+		SubNodeMap=new HashMap();
+	}
+	
+	public void add(FBXNode child){
+		if(child==null)
+			return;
+		if(children==null){
+			children=new ArrayList<FBXNode>();
+		}
+		child.parent=this;
+		this.children.add(child);
+	}
+}
