c789177b7e85409d1610b26922bb95ca98356768
winterarea <87577641+winterarea@users.noreply.github.com>
2022-04-12 10:47:55 +0800

    Create UvConfig.java

---
+++ UvConfig.java
@@ -0,0 +1,17 @@
+package com.nmaid.asset.procedural;
+
+public class UvConfig {
+	private int resolution;
+	private int gird;
+	private int cordType;//origin point opengl:bottom left,direct x:top left
+	private int gridType;//rectangle , roundRectangle , circle
+	private int gridCornerOffset;// left top
+	private int gridCornerScale;
+	
+	private int background;
+	private int textType;//A0-G7,0,0 1,1
+	private int textColor;//white
+	
+	private int gridColorType;//colorful , black/white 
+	
+}
